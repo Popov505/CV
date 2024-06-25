866b4f76acc95f6dbca45cbe8b086918c8a8ba33
Popov505 <ivandelville@gmail.com>
2024-06-25 17:23:09 +0200

    link to style files updated in index.php

--- sources/php/index.php
+++ sources/php/index.php
@@ -7,12 +7,14 @@
       <!--Title-->
       <title>CV de Ivan Delville : Développeur full stack junior - Page d'accueil</title>
       <!-- Fonts Rajdhani and Barlow -->
-      <link rel="preconnect" href="https://fonts.googleapis.com">
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link rel="preload" href="https://fonts.googleapis.com">
+      <link rel="preload" href="https://fonts.gstatic.com" crossorigin>
       <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Roboto+Slab&display=swap" rel="stylesheet">
       <!--Styles : CSS-->
-      <link rel="stylesheet" href="../styles/common.css" type="text/css">
-      <link rel="stylesheet" href="../styles/index.css" type="text/css">
+      <link rel="preload" href="../styles/common.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+      <noscript><link rel="stylesheet" href="../styles/common.css"></noscript>
+      <link rel="preload" href="../styles/index.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+      <noscript><link rel="stylesheet" href="../styles/index.css"></noscript>
       <!--Script : JS-->
       <script defer src="../scripts/index.js"></script>
     </head>
