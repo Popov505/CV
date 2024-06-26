b356fb6c118de41bd559f4a81ec9064acc4ef004
Popov505 <ivandelville@gmail.com>
2024-06-26 16:36:14 +0200

    Test : path modified for header and footer

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
     <body>
       <!--Header-->
       <?php 
-      include '/public/php/header.php'
+      include './header.php'
       ?>
 
       <main>
@@ -134,7 +134,7 @@
 
       <!--Footer-->
       <?php 
-      include '/public/php/footer.php'
+      include '/php/footer.php'
       ?>
      
   </body>
